0a028596b6eb8243ffb370374c00e080323213e2
taleseugenio17-tech <taleseugenio17@gmail.com>
2026-09-05 17:03:09 -0300

    Add files via upload

---
+++ AULA 1/atividade_1.py
@@ -0,0 +1,11 @@
+import streamlit as st
+
+st.header('LAURIMAR  - TALES EUGÊNIO')
+st.write('Mechanical Engineer - Internal Cumbustion Engine')
+
+
+st.write('email: taleseugenio17@gmail.com')
+st.write('celular 55 11 999999999999')
+
+
+
